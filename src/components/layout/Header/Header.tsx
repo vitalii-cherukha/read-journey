0a2a93002c25bac3c390a9deee09273474f0bce4
vitalii_cherukha <vitalii.cherukha@gmail.com>
2026-02-03 13:22:19 +0200

    add styles and markup in header

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -4,7 +4,7 @@ import css from './Header.module.css';
 
 const Header = () => {
   return (
-    <header>
+    <header className={css.header}>
       <Container>
         <div className={css.wrapper}>
           <Link className={css.logo} to="/">
@@ -13,6 +13,19 @@ const Header = () => {
             </svg>
             <span className={css.logoText}>read journey</span>
           </Link>
+          <nav className={css.nav}></nav>
+          <div className={css.actionsWrapper}>
+            <div className={css.userWrapper}>
+              <span className={css.userIcon}>{'I'}</span>
+              <span className={css.userName}>{'Ilona Ratushniak'}</span>
+            </div>
+            <button className={css.menuBtn}>
+              <svg className={css.menuIcon} width="28" height="28">
+                <use href="/sprite.svg#icon-menu" />
+              </svg>
+            </button>
+            <button className={css.actionsBtn}>Log out</button>
+          </div>
         </div>
       </Container>
     </header>
