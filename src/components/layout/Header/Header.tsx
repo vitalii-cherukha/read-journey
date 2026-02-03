e7456749ef2876ba87df3c422d1e4fbd65f96c45
vitalii_cherukha <vitalii.cherukha@gmail.com>
2026-02-03 12:17:37 +0200

    add markup and styles in Header component

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,5 +1,22 @@
+import { Link } from 'react-router';
+import Container from '../../common/Container/Container';
+import css from './Header.module.css';
+
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header>
+      <Container>
+        <div className={css.wrapper}>
+          <Link className={css.logo} to="/">
+            <svg className={css.logoIcon} width="42" height="17">
+              <use href="/sprite.svg#icon-logo" />
+            </svg>
+            <span className={css.logoText}>read journey</span>
+          </Link>
+        </div>
+      </Container>
+    </header>
+  );
 };
 
 export default Header;
