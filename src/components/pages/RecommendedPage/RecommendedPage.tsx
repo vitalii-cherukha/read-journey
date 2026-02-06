d3c1011f9da589814db52d933be34955d8a9c099
vitalii_cherukha <vitalii.cherukha@gmail.com>
2026-02-06 12:42:24 +0200

    add About and Quote components to RecommendedPage

--- src/components/pages/RecommendedPage/RecommendedPage.tsx
+++ src/components/pages/RecommendedPage/RecommendedPage.tsx
@@ -1,6 +1,8 @@
+import About from '../../common/About/About';
 import Container from '../../common/Container/Container';
 import Dashboard from '../../common/Dashboard/Dashboard';
 import Filters from '../../common/Filters/Filters';
+import Quote from '../../common/Quote/Quote';
 import RecommendedBooks from '../../common/RecommendedBooks/RecommendedBooks';
 
 const RecommendedPage = () => {
@@ -9,6 +11,8 @@ const RecommendedPage = () => {
       <Container>
         <Dashboard>
           <Filters />
+          <About />
+          <Quote />
         </Dashboard>
         <RecommendedBooks />
       </Container>
