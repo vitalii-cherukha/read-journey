7b178a1928ea395c161382832a0ed1e7e2a60680
vitalii_cherukha <vitalii.cherukha@gmail.com>
2026-02-10 17:34:27 +0200

    add react-hook-form and react-spiner in filters component

--- src/components/common/Filters/Filters.tsx
+++ src/components/common/Filters/Filters.tsx
@@ -1,15 +1,29 @@
 import { useForm } from 'react-hook-form';
 import css from './Filters.module.css';
+import { BarLoader } from 'react-spinners';
 
-const Filters = () => {
+type FilterData = {
+  title: string;
+  author: string;
+  page: number;
+};
+
+interface FiltersProps {
+  onSubmit: (data: FilterData) => void;
+  loading: boolean;
+}
+
+const Filters = ({ onSubmit, loading }: FiltersProps) => {
+  const { register, handleSubmit } = useForm<FilterData>();
+
   return (
-    <form className={css.form}>
+    <form className={css.form} onSubmit={handleSubmit(onSubmit)}>
       <h3 className={css.title}>Filters:</h3>
       <div className={css.inputWrapper}>
         <label className={css.label}>
           Book title:
           <input
-            name="title"
+            {...register('title')}
             type="text"
             placeholder="I See You Are Interested In The Dark"
           />
@@ -18,17 +32,30 @@ const Filters = () => {
       <div className={css.inputWrapper}>
         <label className={css.label}>
           The author:
-          <input name="author" type="text" placeholder="Hilarion Pavlyuk" />
+          <input
+            {...register('author')}
+            type="text"
+            placeholder="Hilarion Pavlyuk"
+          />
         </label>
       </div>
       <div className={css.inputWrapper}>
         <label className={css.label}>
           Number of pages:
-          <input name="page" type="number" placeholder="664" />
+          <input {...register('page')} type="number" placeholder="664" />
         </label>
       </div>
       <button type="submit" className={css.btn}>
-        Add book
+        {!loading ? (
+          'Add book'
+        ) : (
+          <BarLoader
+            className={css.loader}
+            color="#121417"
+            speedMultiplier={3}
+            width={65}
+          />
+        )}
       </button>
     </form>
   );
