9fecce31ea91b821738c215a23c2f4742c586e52
vitalii_cherukha <vitalii.cherukha@gmail.com>
2026-02-10 17:45:27 +0200

    fix form in filters component

--- src/components/common/Filters/Filters.tsx
+++ src/components/common/Filters/Filters.tsx
@@ -5,7 +5,6 @@ import { BarLoader } from 'react-spinners';
 type FilterData = {
   title: string;
   author: string;
-  page: number;
 };
 
 interface FiltersProps {
@@ -22,32 +21,18 @@ const Filters = ({ onSubmit, loading }: FiltersProps) => {
       <div className={css.inputWrapper}>
         <label className={css.label}>
           Book title:
-          <input
-            {...register('title')}
-            type="text"
-            placeholder="I See You Are Interested In The Dark"
-          />
+          <input {...register('title')} type="text" placeholder="Enter text" />
         </label>
       </div>
       <div className={css.inputWrapper}>
         <label className={css.label}>
           The author:
-          <input
-            {...register('author')}
-            type="text"
-            placeholder="Hilarion Pavlyuk"
-          />
-        </label>
-      </div>
-      <div className={css.inputWrapper}>
-        <label className={css.label}>
-          Number of pages:
-          <input {...register('page')} type="number" placeholder="664" />
+          <input {...register('author')} type="text" placeholder="Enter text" />
         </label>
       </div>
       <button type="submit" className={css.btn}>
         {!loading ? (
-          'Add book'
+          'To apply'
         ) : (
           <BarLoader
             className={css.loader}
