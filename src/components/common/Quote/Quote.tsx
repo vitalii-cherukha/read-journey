1ea734b5ca7a157bd8262d0ecc869cca0d293c18
vitalii_cherukha <vitalii.cherukha@gmail.com>
2026-02-12 09:41:35 +0200

    add styles quote component

--- src/components/common/Quote/Quote.tsx
+++ src/components/common/Quote/Quote.tsx
@@ -1,7 +1,7 @@
 import css from './Quote.module.css';
 
 const Quote = () => {
-  return <div className={css.wrapper}>Quote</div>;
+  return <div className={css.wrapper}>📚</div>;
 };
 
 export default Quote;
