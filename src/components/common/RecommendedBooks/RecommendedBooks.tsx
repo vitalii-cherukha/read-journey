5a7bcb4bae4aace14291ff6031de4734b22abb5a
vitalii_cherukha <vitalii.cherukha@gmail.com>
2026-02-12 10:50:24 +0200

    add styles and markup in quote and recommendedBooks components

--- src/components/common/RecommendedBooks/RecommendedBooks.tsx
+++ src/components/common/RecommendedBooks/RecommendedBooks.tsx
@@ -1,7 +1,15 @@
 import css from './RecommendedBooks.module.css';
 
 const RecommendedBooks = () => {
-  return <div className={css.wrapper}>RecommendedBooks</div>;
+  return (
+    <div className={css.wrapper}>
+      <div className={css.titleWrapper}>
+        <h2 className={css.title}>Recommended</h2>
+        <div className={css.pagination}>{'< >'}</div>
+      </div>
+      <ul className={css.list}></ul>
+    </div>
+  );
 };
 
 export default RecommendedBooks;
