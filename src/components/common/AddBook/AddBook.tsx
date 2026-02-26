dedebc72e8b7aed9b55897a7d356947a80a0ab8a
vitalii_cherukha <vitalii.cherukha@gmail.com>
2026-02-26 15:55:20 +0200

    implement AddBook component with form and styles

--- src/components/common/AddBook/AddBook.tsx
+++ src/components/common/AddBook/AddBook.tsx
@@ -1,5 +1,63 @@
-const AddBook = () => {
-  return <div>AddBook</div>;
+import { useForm } from 'react-hook-form';
+import css from './AddBook.module.css';
+import type { AddBookData } from '../../../types/filter';
+
+interface AddBookProps {
+  onSubmit: (data: { title: string; author: string; number: number }) => void;
+  loading: boolean;
+}
+
+const AddBook = ({ onSubmit, loading }: AddBookProps) => {
+  const { register, handleSubmit } = useForm<AddBookData>();
+
+  return (
+    <form className={css.form} onSubmit={handleSubmit(onSubmit)}>
+      <h3 className={css.title}>Filters:</h3>
+
+      <label className={css.label}>
+        Book title:
+        <input
+          className={css.input}
+          {...register('title')}
+          type="text"
+          placeholder="I See You Are Interested In The Dark"
+        />
+      </label>
+
+      <label className={css.label}>
+        The author:
+        <input
+          className={css.input}
+          {...register('author')}
+          type="text"
+          placeholder="Hilarion Pavlyuk"
+        />
+      </label>
+
+      <label className={css.label}>
+        Number of pages:
+        <input
+          className={css.input}
+          {...register('number')}
+          type="number"
+          placeholder="664"
+        />
+      </label>
+
+      <button type="submit" className={css.btn}>
+        {!loading ? (
+          'Add book'
+        ) : (
+          <BarLoader
+            className={css.loader}
+            color="#121417"
+            speedMultiplier={3}
+            width={65}
+          />
+        )}
+      </button>
+    </form>
+  );
 };
 
 export default AddBook;
--- src/types/filter.ts
+++ src/types/filter.ts
@@ -2,3 +2,9 @@ export type FilterData = {
   title: string;
   author: string;
 };
+
+export type AddBookData = {
+  title: string;
+  author: string;
+  number: number;
+};
