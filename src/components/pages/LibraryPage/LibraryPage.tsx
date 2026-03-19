fd9987e6a6925312adde091391961dc3d1eaee3c
vitalii_cherukha <vitalii.cherukha@gmail.com>
2026-03-19 16:12:48 +0200

    add wrapper styles to LibraryPage for improved layout

--- src/components/pages/LibraryPage/LibraryPage.tsx
+++ src/components/pages/LibraryPage/LibraryPage.tsx
@@ -3,16 +3,19 @@ import Container from '../../common/Container/Container';
 import Dashboard from '../../common/Dashboard/Dashboard';
 import MyLibraryBooks from '../../common/MyLibraryBooks/MyLibraryBooks';
 import RecommendedCard from '../../common/RecommendedCard/RecommendedCard';
+import css from './LibraryPage.module.css';
 
 const LibraryPage = () => {
   return (
     <section>
       <Container>
-        <Dashboard>
-          <AddBook />
-          <RecommendedCard />
-        </Dashboard>
-        <MyLibraryBooks />
+        <div className={css.wrapper}>
+          <Dashboard>
+            <AddBook />
+            <RecommendedCard />
+          </Dashboard>
+          <MyLibraryBooks />
+        </div>
       </Container>
     </section>
   );
